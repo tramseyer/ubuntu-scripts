4c0cb23f1a836db505842e3da07fd08f87c81583
t <t@ramseyer.email>
2024-07-09 19:42:03 +0000

    Added arm64 support to passmark.sh

--- passmark.sh
+++ passmark.sh
@@ -15,21 +15,35 @@ done
 mkdir -p pt_linux
 cd pt_linux
 
-wget -nv --backups=1 http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncursesw5_6.4-2_amd64.deb
-wget -nv --backups=1 http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb
-dpkg-deb -xv libncursesw5_6.4-2_amd64.deb libncursesw5
-dpkg-deb -xv libtinfo5_6.4-2_amd64.deb libtinfo5
-mv libncursesw5/lib/x86_64-linux-gnu/libncursesw.so.5.9 libncurses.so.5
-mv libtinfo5/lib/x86_64-linux-gnu/libtinfo.so.5.9 libtinfo.so.5
+PT_LINUX_ARCH=""
 
-wget -nv --backups=1 https://www.passmark.com/downloads/pt_linux_x64.zip
-python3 -c 'import sys; from zipfile import PyZipFile; PyZipFile(sys.argv[1]).extractall()' pt_linux_x64.zip
-mv PerformanceTest/pt_linux_x64 .
-chmod a+x pt_linux_x64
+if dpkg --print-architecture | grep amd64; then
+    wget -nv --backups=1 http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncursesw5_6.4-2_amd64.deb
+    wget -nv --backups=1 http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb
+    dpkg-deb -xv libncursesw5_6.4-2_amd64.deb libncursesw5
+    dpkg-deb -xv libtinfo5_6.4-2_amd64.deb libtinfo5
+    mv libncursesw5/lib/x86_64-linux-gnu/libncursesw.so.5.9 libncurses.so.5
+    mv libtinfo5/lib/x86_64-linux-gnu/libtinfo.so.5.9 libtinfo.so.5
+    PT_LINUX_ARCH="x64"
+elif dpkg --print-architecture | grep arm64; then
+    wget -nv --backups=1 http://ports.ubuntu.com/pool/main/n/ncurses/libncursesw5_6.1-1ubuntu1_arm64.deb
+    wget -nv --backups=1 http://ports.ubuntu.com/pool/main/n/ncurses/libtinfo5_6.1-1ubuntu1_arm64.deb
+    dpkg-deb -xv libncursesw5_6.1-1ubuntu1_arm64.deb libncursesw5
+    dpkg-deb -xv libtinfo5_6.1-1ubuntu1_arm64.deb libtinfo5
+    mv libncursesw5/lib/aarch64-linux-gnu/libncursesw.so.5.9 libncurses.so.5
+    mv libtinfo5/lib/aarch64-linux-gnu/libtinfo.so.5.9 libtinfo.so.5
+    PT_LINUX_ARCH="arm64"
+fi
 
-TERM=xterm LD_LIBRARY_PATH="$(pwd)" ./pt_linux_x64 -r 1
-grep -E "Processor:|NumCores:|NumLogicals:|Memory:" results_cpu.yml
-grep -E "CPU_SINGLETHREAD|SUMM_CPU" results_cpu.yml
+if [ ! -z "$PT_LINUX_ARCH" ]; then
+    wget -nv --backups=1 "https://www.passmark.com/downloads/pt_linux_$PT_LINUX_ARCH.zip"
+    python3 -c 'import sys; from zipfile import PyZipFile; PyZipFile(sys.argv[1]).extractall()' "pt_linux_$PT_LINUX_ARCH.zip"
+    mv "PerformanceTest/pt_linux_$PT_LINUX_ARCH" .
+    chmod a+x "pt_linux_$PT_LINUX_ARCH"
+    TERM=xterm LD_LIBRARY_PATH="$(pwd)" "./pt_linux_$PT_LINUX_ARCH" -r 1
+    grep -E "Processor:|NumCores:|NumLogicals:|Memory:" results_cpu.yml
+    grep -E "CPU_SINGLETHREAD|SUMM_CPU" results_cpu.yml
+fi
 
 cd ..
 rm -r pt_linux

e55619823ce5d37f91eb96d928fe517c24ac2519
t <t@ramseyer.email>
2025-11-13 23:18:53 +0100

    Adjustments for passmark naming changes

--- passmark.sh
+++ passmark.sh
@@ -24,7 +24,7 @@ if dpkg --print-architecture | grep amd64; then
     dpkg-deb -xv libtinfo5_6.3-2_amd64.deb libtinfo5
     mv libncursesw5/lib/x86_64-linux-gnu/libncursesw.so.5.9 libncurses.so.5
     mv libtinfo5/lib/x86_64-linux-gnu/libtinfo.so.5.9 libtinfo.so.5
-    PT_LINUX_ARCH="x64"
+    PT_LINUX_ARCH="x86-64"
 elif dpkg --print-architecture | grep arm64; then
     wget -nv --backups=1 http://ports.ubuntu.com/pool/main/n/ncurses/libncursesw5_6.1-1ubuntu1_arm64.deb
     wget -nv --backups=1 http://ports.ubuntu.com/pool/main/n/ncurses/libtinfo5_6.1-1ubuntu1_arm64.deb
@@ -32,16 +32,16 @@ elif dpkg --print-architecture | grep arm64; then
     dpkg-deb -xv libtinfo5_6.1-1ubuntu1_arm64.deb libtinfo5
     mv libncursesw5/lib/aarch64-linux-gnu/libncursesw.so.5.9 libncurses.so.5
     mv libtinfo5/lib/aarch64-linux-gnu/libtinfo.so.5.9 libtinfo.so.5
-    PT_LINUX_ARCH="arm64"
+    PT_LINUX_ARCH="ARM64"
 fi
 
 
 if [ ! -z "$PT_LINUX_ARCH" ]; then
-    wget -nv --backups=1 "https://www.passmark.com/downloads/pt_linux_$PT_LINUX_ARCH.zip"
-    python3 -c 'import sys; from zipfile import PyZipFile; PyZipFile(sys.argv[1]).extractall()' "pt_linux_$PT_LINUX_ARCH.zip"
-    mv "PerformanceTest/pt_linux_$PT_LINUX_ARCH" .
-    chmod a+x "pt_linux_$PT_LINUX_ARCH"
-    TERM=xterm LD_LIBRARY_PATH="$(pwd)" "./pt_linux_$PT_LINUX_ARCH" -r 1
+    wget -nv --backups=1 "https://www.passmark.com/downloads/PerformanceTest_Linux_$PT_LINUX_ARCH.zip"
+    python3 -c 'import sys; from zipfile import PyZipFile; PyZipFile(sys.argv[1]).extractall()' "PerformanceTest_Linux_$PT_LINUX_ARCH.zip"
+    mv "PerformanceTest/PerformanceTest_Linux_$PT_LINUX_ARCH" .
+    chmod a+x "PerformanceTest_Linux_$PT_LINUX_ARCH"
+    TERM=xterm LD_LIBRARY_PATH="$(pwd)" "./PerformanceTest_Linux_$PT_LINUX_ARCH" -r 1
     grep -E "Processor:|NumCores:|NumLogicals:|Memory:" results_cpu.yml
     grep -E "CPU_SINGLETHREAD|SUMM_CPU" results_cpu.yml
 fi
